f472429d57d079c1b96ae15a82945da92c042f63
Luclamei16 <luclamei16@gmail.com>
2021-07-16 17:02:04 -0300

    projeto finalizado whatslab5

--- whatslab5/src/App.js
+++ whatslab5/src/App.js
@@ -1,72 +1,6 @@
-// import logo from './logo.svg';
-// import './App.css';
-// import React, { Component } from "react"
-
-// export default class App extends React.Component {
-//   state = {
-//     nome: "",
-//     mensagem: "",
-//     usuario:[]
-
-//   };
-
-//   changeNome = (event) => {
-//   this.setState({
-//     nome: event.target.value
-//   });
-//   };
-
-//   changeMensagem = (event) => {
-//   this.setState({
-//     mensagem: event.target.value
-//   });
-//   };
-
-//   adicionaMensagem = () =>{
-//     const novasMensagens = [
-//   ...this.state.usuario,
-//   {
-//     nome: this.state.nome,
-//     mensagem: this.state.mensagem
-//   }
-//   ]
-
-//   this.setState({
-//   usuario: novasMensagens,
-//   nome: "",
-//   mensagem: ""
-//   })
-// }
-
-//   render() {
-//     const mostraMensagem = this.state.usuario.map((nome, index) => {
-//       return (
-//         <div key={index}>
-//           <div>
-//           <input 
-//           value={this.state.nome}
-//           onChange={this.changeNome}
-//           placeholder="Nome"/>
-                
-//           <input
-//           value={this.state.mensagem}
-//           onChange={this.changeMensagem}
-//           placeholder="Mensagem" />
-
-//           <button onClick={this.novasMensagens}>Enviar</button>
-//           </div>
-//           {mostraMensagem}
-
-//         </div>
-
-//       )
-//     })
-//   }
-
-// };
 import React from "react";
-import { TeacherItem } from "./App.styles";
-import "./styles.css";
+import { Formulario, MensagemItem, Container, CampoMensagem } from "./App.styles";
+import './App.css';
 
 export default class App extends React.Component {
   state = {
@@ -87,14 +21,7 @@ export default class App extends React.Component {
     });
   };
 
-  addTeacher = () => {
-    if (this.state.mensagem === "") {
-      return alert("Idade precisa ser preenchida");
-    }
-
-    if (this.state.nome === "") {
-      return alert("Nome precisa ser preenchido");
-    }
+  addMensagem = () => {
 
     const novaMensagem = [
       ...this.state.usuario,
@@ -111,36 +38,25 @@ export default class App extends React.Component {
     });
   };
 
-  // removeTeacher = (nameToRemove) => {
-  //   const newTeachers = [...this.state.teachers].filter(
-  //     (teacher) => {
-  //       return teacher.name !== nameToRemove
-  //     }
-  //   ) // prettier-ignore
-
-  //   this.setState({
-  //     teachers: newTeachers
-  //   });
-  // };
-
   render() {
     const listaDeEstudantes = this.state
       .usuario.map((elemento, index) => {
         return (
-          <TeacherItem key={index}>
+          <MensagemItem key={index}>
             <div><b>{elemento.nome}:</b></div>
             <div>{elemento.mensagem}</div>
-          </TeacherItem>
+          </MensagemItem>
         );
-    }); // prettier-ignore
-
-    // this.removeTeacher("Caio")
+    }); 
 
     return (
-      <div className="App">
+      
+      <Container>
+        <CampoMensagem>
         {listaDeEstudantes}
+        </CampoMensagem>
         <hr />
-        <div className="formulario">
+        <Formulario>
           <input
             placeholder="Usuário"
             value={this.state.nome}
@@ -152,11 +68,11 @@ export default class App extends React.Component {
             value={this.state.mensagem}
             onChange={this.changeMensagem}
           />
-          <button onClick={this.addTeacher}>Enviar</button>
-        </div>
+          <button onClick={this.addMensagem}>Enviar</button>
+        </Formulario>
         
         
-      </div>
-    ); // prettier-ignore
+      </Container>
+    ); 
   }
 }
--- whatslab5/src/App.styles.js
+++ whatslab5/src/App.styles.js
@@ -1,20 +1,69 @@
 import styled from "styled-components";
 
-export const TeacherItem = styled.li`
+export const MensagemItem = styled.li`
   
   margin: 16px;
   padding: 16px;
-
   display: flex;
+  flex-direction: column;
+  padding: 0.9em 0.8em;
+  border-radius: 0.5em;
+  font-weight: 450;
+  line-height: 1.3;
+  border: 1px solid black;
+  
+  max-width: 20%;
+  min-width: 8%;
+  margin-bottom: 1em;
+  word-wrap: break-word;
+
+  flex-grow: 1;
+  flex-shrink: 1;
+  flex-basis: 0%;
+  -webkit-box-pack: end;
+  
+  
   
   div:nth-child(2) {
-    color: orange;
+    color: blue;
     padding-left:15px;
-  }
-
-  span:nth-child(3) {
-    :hover {
-      cursor: pointer;
-    }
+    
   }
 `;
+
+export const Formulario = styled.div`
+
+
+border: 2px solid black;
+border-radius: 10px;
+padding: 16px;
+display: grid;
+grid-template-columns: 1fr 5fr 1fr;
+align-items: flex-end;
+justify-content: flex-end;
+column-gap: 20px;
+margin: 20px;
+
+`
+
+export const Container = styled.div`
+
+border: 2px solid black;
+border-radius: 10px;
+padding: 10px;
+margin: 16px;
+height: 100%;
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+
+`
+export const CampoMensagem = styled.div`
+    flex: 1 1 0%;
+    display: flex;
+    flex-direction: column;
+    -webkit-box-pack: end;
+    justify-content: flex-end;
+    padding: 20px;
+    
+`
